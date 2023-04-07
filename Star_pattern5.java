674916907adb2ca01cc0f1847942d80ae0c3b3d3
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-07 11:40:51 +0530

    Create Star_pattern5.java
    output : - 
        *
       **
      ***
     ****
    *****

---
+++ Star_pattern5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Star_pattern5 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter any number do you want to print: ");
+		int n = scn.nextInt();
+		int space = n-1;
+		int star=1;
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=space; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=star; j++) {
+				System.out.print("*");
+			}
+			space--;
+			star++;
+			System.out.println();
+		}
+	
+	}
+
+}
