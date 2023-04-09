eae778ed3ad0406d5bedd87ba2e279cd9b7a0182
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-09 21:45:09 +0530

    Create Star_Pattern11.java
    output-->
    *   *
     * * 
      *  
     * * 
    *   *

---
+++ Star_Pattern11.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Star_Pattern11 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter any data :");
+		int n = scn.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				if(i==j||i+j==n+1) {
+					System.out.print('*');
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
