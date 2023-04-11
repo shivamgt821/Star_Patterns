eb52fbcc0a9086b02bdb23743fd91f49cc146f04
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-11 17:23:15 +0530

    Create Star_Pattern21_SnakePattern.java
    output-->
    1 
    3   2 
    4   5   6 
    10   9   8   7 
    11 12 13 14 15

---
+++ Star_Pattern21_SnakePattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Star_Pattern21_SnakePattern {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter any data :");
+		int n = scn.nextInt();
+		int k = 1;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(i>=j) {
+				if(i%2==0) {
+				System.out.print(k+" ");
+				}
+				k++;
+			}
+		}
+			int l = k-1;
+			for(int j=0; j<n; j++) {
+				if(i>=j) {
+					if(i%2==1) {
+						System.out.print(l+" ");  // we cane direct use l--+" "
+					}
+					l--;
+				}	
+			}
+			System.out.println();
+		}
+	}
+}
