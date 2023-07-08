afedaaaafa77e3dd543e35cf28656f6237f384bf
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-08 14:54:27 +0530

    Create HollowLeftTrianglePattern2.java

---
+++ HollowLeftTrianglePattern2.java
@@ -0,0 +1,35 @@
+package com.shivam.narsh;
+
+import java.util.Scanner;
+
+public class HollowLeftTrianglePattern2 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter any num : ");
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		int space = n/2;
+		int star = 1;
+		
+		for(int i=1;i<=n; i++) {
+			
+			for(int j=1; j<=space; j++) System.out.print("  ");
+			
+			for(int j=1; j<=star; j++) System.out.print("* ");
+			
+			System.out.println();
+			
+			if(i<=n/2) {
+				space--;
+				star++;
+			}
+			else {
+				star--;
+				space++;
+			}
+			
+		}
+	}
+
+}
