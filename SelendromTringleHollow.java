03af382a8fa90d3f3cfab85f30bd8936f1a2b4ea
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-12 18:44:37 +0530

    Create SelendromTringleHollow.java

---
+++ SelendromTringleHollow.java
@@ -0,0 +1,21 @@
+public class SelendromTringleHollow {
+
+		public static void main(String[] args) {
+			int n=9;
+			
+			for(int i=1;i<=n;i++) {
+				
+				for(int j=1;j<=n;j++) {
+					if(i==1||i==j||i+j==n+1||i==n) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				System.out.println();
+				
+			}
+		}
+	}
+
