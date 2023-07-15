d46e0867c02f822bde2d627f86ff1ce44334f46e
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-15 19:00:25 +0530

    Create TringleString.java

---
+++ TringleString.java
@@ -0,0 +1,11 @@
+public class TringleString {
+		
+		public static void main(String[] args) {
+			String s="*";
+			for(int i=1;i<=5;i++) {
+				System.out.print(s);
+				System.out.println();
+				s = s+"*";
+			}	
+		}
+	}
